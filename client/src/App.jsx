9d8ad9cdcdf15149575c6952838dd4f434662e9b
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2023-11-07 11:52:19 +0000

    index.html vite build

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+//import viteLogo from '/vite.svg';
 import { ChakraProvider } from '@chakra-ui/react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+//import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  /*
+  root: 'client',
+  build: {
+    emptyOutDir: true,
+    outDir: '../src/index.html',
+  },
+  */
   plugins: [react()],
-  /*server: {
-    hmr: true,
-    watch: {
-      usePolling: true
-    }
-  }*/
 });
